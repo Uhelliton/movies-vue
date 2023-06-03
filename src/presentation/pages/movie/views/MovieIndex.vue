c8c8234185e32aa1cdd15118cfe1d5fd3b25a1b4
Uhelliton Andrade <uhelliton@uol.com.br>
2023-06-03 19:03:08 -0300

    feat(page/movie): create page index movies popular and tendencies

--- src/presentation/pages/movie/views/MovieIndex.vue
+++ src/presentation/pages/movie/views/MovieIndex.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 import CardMovie from '@/presentation/components/cards/CardMovie.vue'
 
@@ -7,20 +7,85 @@ export default defineComponent({
   name: 'MovieIndex',
   components: {
     CardMovie
+  },
+
+  setup() {
+    const formRef = ref(null)
+
+
+    return {
+      formRef,
+    }
   }
 })
 </script>
 
 <template>
   <main class="flex-1 py-10 px-5 sm:px-10">
-    <div class="mt-4 grid grid-cols-2 sm:grid-cols-6 gap-4">
-      <CardMovie
-        v-for="index in [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]"
-        :key="index"
-        :id="15"
-        title="titulo"
-        description="descrição descrição descrição"
-      />
+    <div class="container mx-auto">
+      <form class="rounded-lg bg-white p-6 shadow-md" ref="formRef">
+        <fieldset class="font-light py-2">
+          <legend>Milhões de Filmes, Séries e Pessoas para Descobrir. Explore já.</legend>
+        </fieldset>
+        <div class="flex items-center">
+          <input  type="text" class="w-full rounded-lg border border-gray-400 p-2" placeholder="Buscar por um Filme, Série ou Pessoa..." />
+          <button class="ml-2 rounded-lg bg-blue-500 p-2 text-white hover:bg-blue-600">Pesquisar</button>
+        </div>
+      </form>
     </div>
+
+    <section class="py-4">
+      <h3 class="text-2xl font-medium mb-4">Tendências</h3>
+      <ul class="list-reset flex border-b">
+        <li class="-mb-px mr-1">
+          <a
+            class="bg-white inline-block py-2 px-4 text-blue-dark font-semibold"
+            :class="[{'border-l border-t border-r rounded-t': true}]"
+            href="#">
+            Hoje
+          </a>
+        </li>
+        <li class="-mb-px mr-1">
+          <a class="bg-white inline-block py-2 px-4 text-blue hover:text-blue-darker font-semibold" href="#">Semana</a>
+        </li>
+      </ul>
+
+      <div class="mt-4 grid grid-cols-2 sm:grid-cols-6 gap-4">
+        <CardMovie
+          v-for="index in [0, 1, 2, 3, 4, 0, ]"
+          :key="index"
+          :id="15"
+          title="titulo"
+          description="descrição descrição descrição"
+        />
+      </div>
+    </section>
+
+    <section class="py-4">
+      <h3 class="text-2xl font-medium mb-4">Os Mais Populares</h3>
+      <ul class="list-reset flex border-b">
+        <li class="-mb-px mr-1">
+          <a
+            class="bg-white inline-block py-2 px-4 text-blue-dark font-semibold"
+            :class="[{'border-l border-t border-r rounded-t': true}]"
+            href="#">
+            Hoje
+          </a>
+        </li>
+        <li class="-mb-px mr-1">
+          <a class="bg-white inline-block py-2 px-4 text-blue hover:text-blue-darker font-semibold" href="#">Semana</a>
+        </li>
+      </ul>
+      <div class="mt-4 grid grid-cols-2 sm:grid-cols-6 gap-4">
+        <CardMovie
+          v-for="index in [0, 1, 2, 3, 4, 7]"
+          :key="index"
+          :id="15"
+          title="titulo"
+          description="descrição descrição descrição"
+        />
+      </div>
+    </section>
+
   </main>
 </template>
