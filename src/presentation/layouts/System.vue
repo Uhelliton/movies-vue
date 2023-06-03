e3ee5543040928f5f40aa8ae6787aa39f4d4ca70
Uhelliton Andrade <uhelliton@uol.com.br>
2023-06-03 01:04:53 -0300

    feat(layout): create layout base

---
+++ src/presentation/layouts/System.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import Nav from '@/presentation/components/layouts/Nav.vue'
+import Header from '@/presentation/components/layouts/Header.vue'
+
+export default defineComponent({
+  name: 'SystemLayout',
+  components: { Nav, Header }
+})
+</script>
+
+<template>
+  <div class="min-h-full">
+    <Nav />
+    <main>
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <RouterView />
+      </div>
+    </main>
+  </div>
+</template>
