276e2fe36c0506afd0f8561011c37094a8ad3a63
Uhelliton Andrade <uhelliton@uol.com.br>
2023-06-05 16:42:42 -0300

    feat(component/alert): create component alert

---
+++ src/presentation/components/alert/Alert.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { toRef, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Alert',
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+
+  setup(props) {
+    return {
+      ...toRef(props.message)
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+    <strong class="font-bold">Ops!</strong>
+    <span class="block sm:inline">{{ message }}</span>
+  </div>
+</template>
